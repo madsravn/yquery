032b5468df456b4b2ceb78ae7a4c615923718792
Mads Ravn <madsravn@gmail.com>
2022-01-03 19:01:48 +0100

    Adding first tests

--- src/utility.rs
+++ src/utility.rs
@@ -9,3 +9,33 @@ pub fn contains(smaller: &HashMap<String, String>, bigger: &HashMap<String, Stri
 pub fn contains_keys(keys: &Vec<String>, map: &HashMap<String, String>) -> bool {
     keys.iter().all(|v| map.get(v).is_some())
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn contains_work() {
+        let map_one = HashMap::from([(String::from("foo"), String::from("1")), (String::from("bar"), String::from("2"))]);
+        let map_two = HashMap::from([(String::from("foo"), String::from("1")), (String::from("bar"), String::from("2")), (String::from("baz"), String::from("3"))]);
+        let c_test = contains(&map_one, &map_two);
+        assert_eq!(c_test, true);
+    }
+
+    #[test]
+    fn contains_reject_one() {
+        let map_one = HashMap::from([(String::from("foo"), String::from("1")), (String::from("bar"), String::from("2"))]);
+        let map_two = HashMap::from([(String::from("foo"), String::from("1")), (String::from("bar"), String::from("2")), (String::from("baz"), String::from("3"))]);
+        let c_test = contains(&map_two, &map_one);
+        assert_eq!(c_test, false);
+    }
+
+    #[test]
+    fn contains_reject_two() {
+        let map_one = HashMap::from([(String::from("foo"), String::from("2")), (String::from("bar"), String::from("2"))]);
+        let map_two = HashMap::from([(String::from("foo"), String::from("1")), (String::from("bar"), String::from("2")), (String::from("baz"), String::from("3"))]);
+        let c_test = contains(&map_two, &map_one);
+        assert_eq!(c_test, false);
+    }
+
+}
